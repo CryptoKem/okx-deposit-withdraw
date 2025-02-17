20c7d8609b45d745b72d34f425391d3ae4a1ed06
Max Zarev <maksimelizarev@gmail.com>
2025-02-17 12:30:32 +0700

    добавлен метод metamask.universal_confirm и исправлено форматирование ковычек

--- core/browser/metamask.py
+++ core/browser/metamask.py
@@ -8,6 +8,7 @@ from models.account import Account
 from models.chain import Chain
 from utils.utils import random_sleep, generate_password, write_text_to_file
 
+
 class Metamask:
     """
     Класс для работы с metamask v. 12.11.0
@@ -31,14 +32,15 @@ class Metamask:
     def create_wallet(self, save_in_excel: bool = False) -> tuple[str, str, str]:
         """
         Создает кошелек в metamask, возвращает адрес кошелька, seed фразу и пароль в виде кортежа.
-        :param safe_in_excel: если True, то адрес, seed и пароль будут записаны в excel файл
+        :param save_in_excel: если True, то адрес, seed и пароль будут записаны в excel файл account.xlsx
         :return: tuple (address, seed, password)
         """
         self.open_metamask()
         try:
             self.ads.page.get_by_test_id('onboarding-terms-checkbox').wait_for(timeout=5000, state='visible')
         except:
-            raise Exception(f"Ошибка создания кошелька {self.ads.profile_number} вероятно в метамаске уже есть счет, обнулите расширение")
+            raise Exception(
+                f'Ошибка создания кошелька {self.ads.profile_number} вероятно в метамаске уже есть счет, обнулите расширение')
 
         self.ads.page.get_by_test_id('onboarding-terms-checkbox').click()
         self.ads.page.get_by_test_id('onboarding-create-wallet').click()
@@ -57,7 +59,7 @@ class Metamask:
 
         seed = []
         for i in range(12):
-            test_id = f"recovery-phrase-chip-{i}"
+            test_id = f'recovery-phrase-chip-{i}'
             word = self.ads.page.get_by_test_id(test_id).inner_text()
             seed.append(word)
 
@@ -75,7 +77,7 @@ class Metamask:
 
         address = self.get_address()
 
-        seed_str = " ".join(seed)
+        seed_str = ' '.join(seed)
 
         if save_in_excel:
             self.excel.set_cell('Address', address)
@@ -93,7 +95,7 @@ class Metamask:
         """
         self.open_metamask()
         if not self.password:
-            raise Exception(f"{self.ads.profile_number} не указан пароль для авторизации в metamask")
+            raise Exception(f'{self.ads.profile_number} не указан пароль для авторизации в metamask')
 
         try:
             self.ads.page.get_by_test_id('unlock-password').wait_for(timeout=5000, state='visible')
@@ -102,12 +104,14 @@ class Metamask:
             random_sleep(3, 5)
             self.ads.click_if_exists(method='test_id', value='popover-close')
         except:
-            logger.warning(f"{self.ads.profile_number} не смогли авторизоваться в metamask, вероятно уже авторизованы")
+            logger.warning(
+                f'{self.ads.profile_number} не смогли авторизоваться в metamask, вероятно уже авторизованы')
 
         if self.ads.page.get_by_test_id('account-options-menu-button').count():
-            logger.info(f"{self.ads.profile_number} успешно авторизован в metamask")
+            logger.info(f'{self.ads.profile_number} успешно авторизован в metamask')
         else:
-            logger.error(f"{self.ads.profile_number} ошибка авторизации в metamask, не смогли войти в кошелек")
+            logger.error(
+                f'{self.ads.profile_number} ошибка авторизации в metamask, не смогли войти в кошелек')
 
     def import_wallet(self) -> tuple[str, str, str]:
         """
@@ -116,7 +120,7 @@ class Metamask:
         """
         self.open_metamask()
 
-        seed_list = self.seed.split(" ")
+        seed_list = self.seed.split(' ')
         if not self.password:
             self.password = generate_password()
         try:
@@ -125,7 +129,7 @@ class Metamask:
             self.ads.page.get_by_test_id('onboarding-import-wallet').click()
             self.ads.page.get_by_test_id('metametrics-no-thanks').click()
             for i, word in enumerate(seed_list):
-                self.ads.page.get_by_test_id(f"import-srp__srp-word-{i}").fill(word)
+                self.ads.page.get_by_test_id(f'import-srp__srp-word-{i}').fill(word)
             self.ads.page.get_by_test_id('import-srp-confirm').click()
             self.ads.page.get_by_test_id('create-password-new').fill(self.password)
             self.ads.page.get_by_test_id('create-password-confirm').fill(self.password)
@@ -136,10 +140,11 @@ class Metamask:
             self.ads.page.get_by_test_id('pin-extension-next').click()
             self.ads.click_if_exists(method='test_id', value='pin-extension-done')
         except:
-            logger.warning(f"{self.ads.profile_number} в метамаске уже имеется счет, делаем сброс и импортируем новый")
+            logger.warning(
+                f'{self.ads.profile_number} в метамаске уже имеется счет, делаем сброс и импортируем новый')
             self.ads.page.get_by_text('Forgot password?').click()
             for i, word in enumerate(seed_list):
-                self.ads.page.get_by_test_id(f"import-srp__srp-word-{i}").fill(word)
+                self.ads.page.get_by_test_id(f'import-srp__srp-word-{i}').fill(word)
             self.ads.page.get_by_test_id('create-vault-password').fill(self.password)
             self.ads.page.get_by_test_id('create-vault-confirm-password').fill(self.password)
             self.ads.page.get_by_test_id('create-new-vault-submit-button').click()
@@ -147,7 +152,7 @@ class Metamask:
         random_sleep(3, 3)
         self.ads.click_if_exists(method='test_id', value='popover-close')
         address = self.get_address()
-        seed_str = " ".join(seed_list)
+        seed_str = ' '.join(seed_list)
         return address, seed_str, self.password
 
     def get_address(self) -> str:
@@ -165,6 +170,7 @@ class Metamask:
         """
         Подтверждает подключение к metamask.
         :param locator: локатор кнопки подключения, после нажатия которой появляется окно метамаска.
+        :param timeout: время ожидания открытия страницы подключения метамаска в секундах, по умолчанию 30
         :return: None
         """
         try:
@@ -173,10 +179,10 @@ class Metamask:
             metamask_page = page_catcher.value
         except Exception as e:
             logger.warning(
-                f"{self.ads.profile_number}  Wargning: не смогли поймать окно метамаск, пробуем еще {e}")
+                f'{self.ads.profile_number}  Wargning: не смогли поймать окно метамаск, пробуем еще {e}')
             metamask_page = self.ads.catch_page(['notification', 'connect', 'confirm-transaction'])
             if not metamask_page:
-                raise Exception(f"Error: {self.ads.profile_number} Ошибка подключения метамаска")
+                raise Exception(f'Error: {self.ads.profile_number} Ошибка подключения метамаска')
 
         metamask_page.wait_for_load_state('load')
 
@@ -186,6 +192,7 @@ class Metamask:
         """
         Подтверждает подпись в metamask.
         :param locator: локатор кнопки вызова подписи.
+        :param timeout: время ожидания открытия страницы подписи в секундах, по умолчанию 30 секунд
         :return: None
         """
         try:
@@ -195,7 +202,7 @@ class Metamask:
         except:
             metamask_page = self.ads.catch_page(['confirm-transaction'])
             if not metamask_page:
-                raise Exception(f"Error: {self.ads.profile_number} Ошибка подписи сообщения в метамаске)")
+                raise Exception(f'Error: {self.ads.profile_number} Ошибка подписи сообщения в метамаске)')
 
         metamask_page.wait_for_load_state('load')
 
@@ -219,15 +226,18 @@ class Metamask:
         except:
             metamask_page = self.ads.catch_page(['notification', 'confirm-transaction'])
             if not metamask_page:
-                raise Exception(f"Error: {self.ads.profile_number} Ошибка подтверждения транзакции метамаска")
+                raise Exception(f'Error: {self.ads.profile_number} Ошибка подтверждения транзакции метамаска')
 
         metamask_page.wait_for_load_state('load')
         confirm_button = metamask_page.get_by_test_id('confirm-footer-button')
-        if not confirm_button.count():
-            confirm_button = metamask_page.get_by_test_id('page-container-footer-next')
 
         confirm_button.click()
 
+        try:
+            if confirm_button.count():
+                confirm_button.click()
+        except:
+            pass
 
     def select_chain(self, chain: Chain) -> None:
         """
@@ -236,8 +246,8 @@ class Metamask:
         :return: None
         """
         self.open_metamask()
-        self.ads.page.get_by_test_id("network-display").wait_for(timeout=5000, state='visible')
-        chain_button = self.ads.page.get_by_test_id("network-display")
+        self.ads.page.get_by_test_id('network-display').wait_for(timeout=5000, state='visible')
+        chain_button = self.ads.page.get_by_test_id('network-display')
         if chain.metamask_name == chain_button.inner_text():
             return
 
@@ -247,11 +257,11 @@ class Metamask:
         if enabled_networks.get_by_text(chain.metamask_name, exact=True).count():
             enabled_networks.get_by_text(chain.metamask_name, exact=True).click()
         else:
-            close_button = self.ads.page.locator('header').get_by_role('button', name='Close').or_(self.ads.page.locator('header').get_by_role('button', name='Закрыть'))
+            close_button = self.ads.page.locator('header').get_by_role('button', name='Close').or_(
+                self.ads.page.locator('header').get_by_role('button', name='Закрыть'))
             close_button.first.click()
             self.set_chain(chain)
             self.select_chain(chain)
-
 
     def _set_chain_data(self, chain: Chain) -> None:
         """
@@ -265,14 +275,12 @@ class Metamask:
         self.ads.page.get_by_test_id('rpc-url-input-test').fill(chain.rpc)
         self.ads.page.get_by_role('button', name='Add URL').click()
 
-
     def set_chain(self, chain: Chain) -> None:
         """
         Добавляет новую сеть в metamask. Берет параметры из объекта Chain.
         :param chain: объект сети
-        :return: None
         """
-        self.ads.open_url(self._url + "#settings/networks/add-network")
+        self.ads.open_url(self._url + '#settings/networks/add-network')
         random_sleep(1, 3)
         self.ads.page.get_by_test_id('network-form-network-name').wait_for(timeout=5000, state='visible')
 
@@ -282,19 +290,20 @@ class Metamask:
         # проверяем, есть ли ошибка с chain_id
         if self.ads.page.get_by_test_id('network-form-chain-id-error').count():
             raise Exception(
-                f"Error: {self.ads.profile_number} metamask не принимает rpc {chain.rpc}, попробуйте другой")
+                f'Error: {self.ads.profile_number} metamask не принимает rpc {chain.rpc}, попробуйте другой')
 
         # заполняем chain_id и проверяем, есть ли уже сеть с таким id
         self.ads.page.get_by_test_id('network-form-chain-id').fill(str(chain.chain_id))
         # есть ли уже сеть с таким id
-        if self.ads.page.get_by_text('This Chain ID is currently used by the Arbitrum One network.').count():
+        if self.ads.page.get_by_text('This Chain ID is currently used by the').count():
             # повторно заполняем поля
             self.ads.page.get_by_role('button', name='edit the original network').click()
             self._set_chain_data(chain)
 
         # заполняем оставшиеся поля
         self.ads.page.get_by_test_id('network-form-ticker-input').fill(chain.native_token)
-        self.ads.page.get_by_role('button', name='Save').or_(self.ads.page.get_by_role('button', name='Сохранить')).click()
+        self.ads.page.get_by_role('button', name='Save').or_(
+            self.ads.page.get_by_role('button', name='Сохранить')).click()
 
     def change_chain_data(self, chain: Chain) -> None:
         """
@@ -305,7 +314,6 @@ class Metamask:
             - chain_id (int): Идентификатор сети (целое число).
             - rpc (str): URL RPC-сервера для подключения.
             - native_token (str): Тикер нативного токена сети.
-        :return: None
         """
         self.open_metamask()
         self.ads.page.get_by_test_id('network-display').click()
@@ -315,7 +323,8 @@ class Metamask:
 
         # Находим нужную сеть и открываем настройки
         if not self.ads.page.get_by_test_id(f'network-list-item-options-button-{hex_id}').count():
-            logger.info(f'{self.ads.profile_number} Сеть {chain.metamask_name} не найдена в списке установленных. Устанавливаем.')
+            logger.info(
+                f'{self.ads.profile_number} Сеть {chain.metamask_name} не найдена в списке установленных. Устанавливаем.')
             self.ads.page.get_by_role('button', name='Close').first.click()
             self.set_chain(chain)
             return
@@ -332,7 +341,7 @@ class Metamask:
         rpc_element = self.ads.page.locator('//button[@data-testid="test-add-rpc-drop-down"]/../..')
         if rpc_element.get_attribute('data-original-title') != chain.rpc:
             rpc_element.click()
-            # Чтобы rpc находилось в списке, убираем "https://"
+            # Чтобы rpc находилось в списке, убираем 'https://'
             rpc_without_https = chain.rpc.replace('https://', '')
             if self.ads.page.get_by_role('tooltip').get_by_text(rpc_without_https).count():
                 self.ads.page.get_by_role('tooltip').get_by_text(rpc_without_https).click()
@@ -347,5 +356,28 @@ class Metamask:
             self.ads.page.get_by_test_id('network-form-ticker-input').fill(chain.native_token)
 
         # Сохраняем изменения
-        self.ads.page.get_by_role('button', name='Save').or_(self.ads.page.get_by_role('button', name='Сохранить')).click()
+        self.ads.page.get_by_role('button', name='Save').or_(
+            self.ads.page.get_by_role('button', name='Сохранить')).click()
         logger.info(f'{self.ads.profile_number} Данные сети {chain.metamask_name} успешно изменены')
+
+    def universal_confirm(self, windows: int = 1, buttons: int = 1) -> None:
+        """
+        Подтверждает действие в metamask. Подходит для любых действий, где нужно подтвердить транзакцию.
+        Запускать после нажатия кнопки вызова расширения metamask.
+        Если не находит кнопку подтверждения, закрывает окно metamask.
+        Можно безопасно вызывать многократно.
+        :param windows: количество окон metamask, которые будут открываться одно за другим
+        :param buttons: количество кнопок подтверждения, которые будут нажаты в пределах каждого окна
+        """
+        for _ in range(windows):
+            random_sleep(2, 3)
+            mm_page = self.ads.context.new_page()
+            mm_page.goto(self._url)
+            buttons_name = ['confirm-btn', 'confirm-footer-button']
+            for __ in range(buttons):
+                for button in buttons_name:
+                    if mm_page.get_by_test_id(button).count():
+                        mm_page.get_by_test_id(button).click()
+                        logger.info(f'{self.ads.profile_number} Успешно подтверждено в metamask')
+                        break
+            mm_page.close()

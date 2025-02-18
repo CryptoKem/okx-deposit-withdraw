9f1e748e50c0e392b6f78e6420efc2b7f057967d
Max Zarev <maksimelizarev@gmail.com>
2025-02-18 13:23:23 +0700

    добавление документации

--- core/exchanges/exchanges.py
+++ core/exchanges/exchanges.py
@@ -4,7 +4,10 @@ from models.account import Account
 
 
 class Exchanges:
-
+    """
+    Класс для работы с биржами. Инициализирует все биржи.
+    При создании новых бирж, импортируйте в данный модуль и инициализируйте в этом классе.
+    """
     def __init__(self, account: Account) -> None:
         self.okx = Okx(account)
         self.binance = Binance(account)
